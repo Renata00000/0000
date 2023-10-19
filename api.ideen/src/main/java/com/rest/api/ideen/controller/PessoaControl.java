77285b9f2d1d2057e975ffaa2a4c145b641dd749
Renata <125274790+Renata00000@users.noreply.github.com>
2023-10-18 23:07:43 -0300

    Update PessoaControl.java
    Atualizando

--- api.ideen/src/main/java/com/rest/api/ideen/controller/PessoaControl.java
+++ api.ideen/src/main/java/com/rest/api/ideen/controller/PessoaControl.java
@@ -3,15 +3,14 @@ package com.rest.api.ideen.controller;
 import com.rest.api.ideen.repository.PessoaRepository;
 import com.rest.api.ideen.entity.Pessoas;
 import com.rest.api.ideen.util.ValidaCpf;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.Optional;
 
@@ -25,27 +24,27 @@ public class PessoaControl {
 
 
     @GetMapping
-    public ResponseEntity<Page<Pessoas>> getAllPessoa(Pageable page) {
+    public ResponseEntity<?> getAllPessoa(Pageable page) {
         Page<Pessoas> pessoas = repository.findAll(page);
         if (pessoas.isEmpty()) {
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No records registered");
         }
         return ResponseEntity.ok(pessoas);
+
+
     }
-
-
-
     @GetMapping("/{id}")
-    public ResponseEntity<Pessoas> getPessoasById(@PathVariable Long id){
+    public ResponseEntity<?> getPessoasById(@PathVariable Long id){
          Optional<Pessoas> pessoa = repository.findById( id);
          if (pessoa.isPresent()){
              return ResponseEntity.ok().body(pessoa.get());
          }else{
-             return ResponseEntity.notFound().build();
+             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Record not found.");
+
+
          }
 
     }
-
 
 
     @PostMapping
@@ -64,51 +63,33 @@ public class PessoaControl {
 
 
     @PutMapping("/{id}")
-                public Pessoas atualizaPessoas(@PathVariable Long id, @RequestBody  Pessoas pessoas){
-            Pessoas p = repository.findById(id).orElse(null);
-            if (pessoas.getNome()!= null) {
+    public ResponseEntity<?> atualizaPessoas(@PathVariable Long id, @RequestBody Pessoas pessoas) {
+        Optional<Pessoas> optionalPessoa = repository.findById(id);
+
+        if (optionalPessoa.isPresent()) {
+            Pessoas p = optionalPessoa.get();
+
+            if (pessoas.getNome() != null) {
                 p.setNome(pessoas.getNome());
             }
-            if (pessoas.getIdade()!= 0) {
+            if (pessoas.getIdade() != 0) {
                 p.setIdade(pessoas.getIdade());
             }
-
-            if (pessoas.getCpf()!= null){
-                    p.setCpf(pessoas.getCpf());
+            if (pessoas.getCpf() != null) {
+                p.setCpf(pessoas.getCpf());
             }
-
-            if (pessoas.getNacionalidade()!= null) {
+            if (pessoas.getNacionalidade() != null) {
                 p.setNacionalidade(pessoas.getNacionalidade());
             }
-            return repository.save(p);
 
+            Pessoas updatedPessoa = repository.save(p);
+
+            return ResponseEntity.ok(updatedPessoa);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(" Record not found.");
+
+        }
     }
-//@PutMapping("/{id}")
-//public ResponseEntity<?> atualizaPessoas(@PathVariable Long id, @RequestBody Pessoas pessoas) {
-//    Optional<Pessoas> optionalPessoa = repository.findById(id);
-//
-//    if (optionalPessoa.isPresent()) {
-//        Pessoas p = optionalPessoa.get();
-//
-//        if (pessoas.getNome() != null) {
-//            p.setNome(pessoas.getNome());
-//        }
-//        if (pessoas.getIdade() != 0) {
-//            p.setIdade(pessoas.getIdade());
-//        }
-//        if (pessoas.getCpf() != null) {
-//            p.setCpf(pessoas.getCpf());
-//        }
-//        if (pessoas.getNacionalidade() != null) {
-//            p.setNacionalidade(pessoas.getNacionalidade());
-//        }
-//        return repository.save(p);
-//        return ResponseEntity.ok().build();
-//
-//    } else {
-//        return ResponseEntity.badRequest().body("Registro não localizado");
-//    }
-//}
 
 
 
@@ -116,17 +97,21 @@ public class PessoaControl {
     @DeleteMapping("/{id}")
 public ResponseEntity<String> deletaPessoas(@PathVariable Long id) {
     Optional<Pessoas> pessoa = repository.findById(id);
-
     if (pessoa.isPresent()) {
         repository.deleteById(id);
         return ResponseEntity.ok("Record deleted!");
     } else {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Register not found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Record not found");
 
     }
 
     }
   }
+
+
+
+
+
 
 
 

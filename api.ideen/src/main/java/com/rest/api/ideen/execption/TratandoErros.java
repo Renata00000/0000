706ee5f07c654a156ff4cd755d4aaeac074878c1
Renata <125274790+Renata00000@users.noreply.github.com>
2023-10-18 23:10:30 -0300

    Update TratandoErros.java
    atualizando

--- api.ideen/src/main/java/com/rest/api/ideen/execption/TratandoErros.java
+++ api.ideen/src/main/java/com/rest/api/ideen/execption/TratandoErros.java
@@ -18,23 +18,19 @@ public class TratandoErros {
         }
 
         @ExceptionHandler(MethodArgumentNotValidException.class)
-        //declarando a exception na assinatura do metodo para ela me devolver o erro com json, se fizer como o acima, apenas me retorna o numero do erro
         public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
-            var erros = ex.getFieldErrors();//getFiedErro, me devolve a lista dos objs que deram erro
+            var erros = ex.getFieldErrors();
             return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
         }
 
-        //dto...dados da dto add apos erros. iniciando strean
+        //DTO
     private record DadosErroValidacao(String campo, String mesagem){
             public  DadosErroValidacao(FieldError erro){
                 this(erro.getField(), erro.getDefaultMessage());
-            }              //campo          mesagen
+            }                       
 
     }
 
-        @ExceptionHandler(HttpMessageNotReadableException.class)
-        public ResponseEntity tratarErro400(HttpMessageNotReadableException ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+       
 
-}
+
